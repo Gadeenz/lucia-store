8b6da6f4ff1d6818cfe673a1c56f712a9a3c1980
gadeenz <mohammedgadeen@gmail.com>
2023-12-09 14:01:35 +0400

    add content loader component

--- src/Components/index.tsx
+++ src/Components/index.tsx
@@ -1,3 +1,4 @@
 export { Footer } from './Footer/Footer';
 export { Navbar } from './Navbar/Navbar';
 export { SectionHeader } from './Sectionheader/SectionHeader';
+export { ContentLoader } from './ContentLoader/ContentLoader';
